65182642db90c225b8386e4c5c40690ee648abd2
Stephen Shilale <sshilale@thomasnet.com>
2018-02-22 12:01:46 -0500

    s4: added sky, made canvas fit whole page

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Sky from './Sky';
 
 const Canvas = () => {
-  const style = {
-    border: '1px solid black',
-  };
+  // viewbox defines that your canvas and contents must fit a particular container
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
+    // SVG does not use z-index, so elements must be defined in stacking order.. i.e, sky is background so it goes first
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
-      style={style}
+      viewBox={viewBox}
     >
-      <circle cx={25} cy={25} r={10} />
+      <Sky />
+      <circle cx={0} cy={0} r={50} />
     </svg>
   );
 };
