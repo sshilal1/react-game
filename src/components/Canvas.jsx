560143a6fb66a82b8e6991d334db59870d162c7d
Stephen Shilale <sshilale@thomasnet.com>
2018-02-22 14:19:18 -0500

    s6: make the cannon follow the mouse

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
-  // viewbox defines that your canvas and contents must fit a particular container
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
-    // SVG does not use z-index, so elements must be defined in stacking order.. i.e, sky is background so it goes first
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
     </svg>
   );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
