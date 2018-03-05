168730a7d71c50a201febfb2ea283a46a4f21e70
Stephen Shilale <sshilale@thomasnet.com>
2018-03-05 17:20:12 -0500

    s11: added random generated flying objects after user starts game

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -37,11 +37,12 @@ const Canvas = (props) => {
         </g>
       }
 
-      { props.gameState.started &&
-        <g>
-          <FlyingObject position={{x: 0, y: -300}}/>
-        </g>
-      }
+      { props.gameState.flyingObjects.map(flyingObject => (
+        <FlyingObject
+          key={flyingObject.id}
+          position={flyingObject.position}
+        />
+      ))}
     </svg>
   );
 };
@@ -52,6 +53,13 @@ Canvas.propTypes = {
     started: PropTypes.bool.isRequired,
     kills: PropTypes.number.isRequired,
     lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
   }).isRequired,
   trackMouse: PropTypes.func.isRequired,
   startGame: PropTypes.func.isRequired,
