3c7d01b446161f9205a2e4affbec8578acf4a164
Stephen Shilale <sshilale@thomasnet.com>
2018-05-11 14:24:43 -0400

    s14: added collisions

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -1,6 +1,7 @@
 import { calculateAngle } from "../utils/formulas";
 import createFlyingObjects from "./createFlyingObjects";
 import moveBalls from './moveCannonBalls';
+import checkCollisions from './checkCollisions';
 
 function moveObjects(state, action) {
 
@@ -16,9 +17,16 @@ function moveObjects(state, action) {
   const newState = createFlyingObjects(state);
 
   const now = new Date().getTime();
-  const flyingObjects = newState.gameState.flyingObjects.filter(
+  let flyingObjects = newState.gameState.flyingObjects.filter(
     object => now - object.createdAt < 4000
   );
+
+  const objectsDestroyed = checkCollisions(cannonBalls, flyingObjects);
+  const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
+  const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
+
+  cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
+  flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
 
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
